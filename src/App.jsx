511aad6d33df9b6a7737e5c34f0b1d0cbdcc07dd
Asmit tyagi <ashmittyagigms@gmail.com>
2025-11-26 21:15:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-2xl'>Hi Asmit Time to reboot.</div>
+    <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+const Header = () => {
+  return (
+    <div>Every This is going to be ok, brother. Just hang in there and do the work bro. </div>
+  )
+}
+
+export default Header
